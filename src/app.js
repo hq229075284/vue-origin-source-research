d1d7358381d5ad30c79e0b3a0adf56ddd3936bde
hanqing <229075284@qq.com>
2021-02-28 20:39:00 +0800

    update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import main from './main'
 import component1 from './component1'
 // import router from './router'
-
+debugger
 new Vue({
     el:document.querySelector('#app'),
     render:(h)=>h(component1),
