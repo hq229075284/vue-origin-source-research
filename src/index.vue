1b9a564bd7be5ce186d54effa97a5ca5c649ce64
hanqing <hanq@example.com>
2022-05-18 17:29:28 +0800

    update

--- src/index.vue
+++ src/index.vue
@@ -1,67 +1,7 @@
 <template>
-    <div>a的值：{{a}}{{object.foo}}</div>
+    <div><input type="text" v-model="valueA"></div>
 </template>
-
 <script setup>
-import {ref,reactive} from 'vue'
-const a=ref(1)
-
-debugger
-const b = reactive({
-  a
-})
-
-b.a=a
-
-// Object(vue__WEBPACK_IMPORTED_MODULE_0__["toDisplayString"])($setup.object.foo) <- 👈render
-
-// const toDisplayString = (val) => {
-//     return isString(val)
-//         ? val
-//         : val == null
-//             ? ''
-//             : isArray(val) ||
-//                 (isObject(val) &&
-//                     (val.toString === objectToString || !isFunction(val.toString)))
-//                 ? JSON.stringify(val, replacer, 2) <- 👈拆解ref
-//                 : String(val);
-// };
-// const replacer = (_key, val) => {
-//     // can't use isRef here since @vue/shared has no deps
-//     if (val && val.__v_isRef) {
-//         return replacer(_key, val.value); <- 👈拆解ref
-//     }
-//     else if (isMap(val)) {
-//         return {
-//             [`Map(${val.size})`]: [...val.entries()].reduce((entries, [key, val]) => {
-//                 entries[`${key} =>`] = val;
-//                 return entries;
-//             }, {})
-//         };
-//     }
-//     else if (isSet(val)) {
-//         return {
-//             [`Set(${val.size})`]: [...val.values()]
-//         };
-//     }
-//     else if (isObject(val) && !isArray(val) && !isPlainObject(val)) {
-//         return String(val);
-//     }
-//     return val;
-// };
-const object = { foo: ref(2) }
-const {foo}=object
-
-const state=ref(1)
-state.value={y:2}
-</script>
-
-<script>
-export default {
-    data(){
-        return {
-            a:1
-        }
-    }
-}
+import {ref} from 'vue'
+const valueA=ref('1')
 </script>
